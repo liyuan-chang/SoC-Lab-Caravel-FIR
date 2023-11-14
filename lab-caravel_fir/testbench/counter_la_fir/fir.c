e28a5d7f95b814d001f35e8392e6aadd7d82b2e9
liyuan-chang <lychang.ee@gmail.com>
2023-11-14 21:56:15 +0800

    caravel-fir: add tb, fir.c, fir.h, rtl.list

--- lab-caravel_fir/testbench/counter_la_fir/fir.c
+++ lab-caravel_fir/testbench/counter_la_fir/fir.c
@@ -1,12 +1,56 @@
 #include "fir.h"
 
 void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
-	//initial your fir
+	// initial your fir
+
+	// reset control registers
+	reg_fir_control = 0;
+
+	// program coefficients and data length
+	// volatile uint32_t* coeff_ptr = &reg_fir_coeff;
+	// for (int i = 0; i < N; ++i) {
+	// 	*(coeff_ptr + i) = taps[i];
+	// }
+	reg_fir_coeff_0  = taps[0];
+	reg_fir_coeff_1  = taps[1];
+	reg_fir_coeff_2  = taps[2];
+	reg_fir_coeff_3  = taps[3];
+	reg_fir_coeff_4  = taps[4];
+	reg_fir_coeff_5  = taps[5];
+	reg_fir_coeff_6  = taps[6];
+	reg_fir_coeff_7  = taps[7];
+	reg_fir_coeff_8  = taps[8];
+	reg_fir_coeff_9  = taps[9];
+	reg_fir_coeff_10 = taps[10];
+	reg_fir_data_len = N;
 }
 
 int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
 	initfir();
-	//write down your fir
+	// write down your fir
+
+	// assert ap_start
+	reg_fir_control = 0x00000001;
+
+	// send x to fir and receive y from fir
+	// x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+	for (int i = 0; i < N; ++i) {
+		while (1) {
+			// wait for fir to be ready to accept input
+			if ((reg_fir_control & 0x00000010) == 0x00000010) {
+				reg_fir_x = (i + 1);
+				break;
+			}
+		}
+		while (1) {
+			// wait for the output of fir
+			if ((reg_fir_control & 0x00000020) == 0x00000020) {
+				outputsignal[i] = reg_fir_y;
+				break;
+			}
+		}
+	}
+
 	return outputsignal;
 }
 		
--- lab-caravel_fir/testbench/counter_la_fir/fir.h
+++ lab-caravel_fir/testbench/counter_la_fir/fir.h
@@ -1,7 +1,25 @@
 #ifndef __FIR_H__
 #define __FIR_H__
 
+#include <stdint.h>
+
 #define N 11
+
+#define reg_fir_control  (*(volatile uint32_t*)0x30000000)
+#define reg_fir_data_len (*(volatile uint32_t*)0x30000010)
+#define reg_fir_coeff_0  (*(volatile uint32_t*)0x30000040)
+#define reg_fir_coeff_1  (*(volatile uint32_t*)0x30000044)
+#define reg_fir_coeff_2  (*(volatile uint32_t*)0x30000048)
+#define reg_fir_coeff_3  (*(volatile uint32_t*)0x3000004c)
+#define reg_fir_coeff_4  (*(volatile uint32_t*)0x30000050)
+#define reg_fir_coeff_5  (*(volatile uint32_t*)0x30000054)
+#define reg_fir_coeff_6  (*(volatile uint32_t*)0x30000058)
+#define reg_fir_coeff_7  (*(volatile uint32_t*)0x3000005c)
+#define reg_fir_coeff_8  (*(volatile uint32_t*)0x30000060)
+#define reg_fir_coeff_9  (*(volatile uint32_t*)0x30000064)
+#define reg_fir_coeff_10 (*(volatile uint32_t*)0x30000068)
+#define reg_fir_x        (*(volatile uint32_t*)0x30000080)
+#define reg_fir_y        (*(volatile uint32_t*)0x30000084)
 
 int taps[N] = {0,-10,-9,23,56,63,56,23,-9,-10,0};
 int inputbuffer[N];
